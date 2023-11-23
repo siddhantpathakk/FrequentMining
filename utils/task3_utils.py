160be6737b6cb72eecb91fbc7e383ef920b8aa53
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-11-23 22:48:39 +0800

    add: support for 2d,3d plots

--- utils/task3_utils.py
+++ utils/task3_utils.py
@@ -249,34 +249,31 @@ def plot_clusters(df_new, clusters, target):
 def plot_clusters_3d(df_new, clusters, target):
     pca = PCA(n_components=3)
     trans_data_pca = pca.fit_transform(df_new)
-    fig = plt.figure(figsize=(16, 8))
+    fig = plt.figure(figsize=(24, 8))
     
     ax = fig.add_subplot(121, projection='3d')
-
     scatter = ax.scatter(trans_data_pca[:, 0], trans_data_pca[:, 1],
                          trans_data_pca[:, 2], c=clusters, cmap='viridis', s=50)
     
     ax.set_xlabel('Principal Component 1')
     ax.set_ylabel('Principal Component 2')
     ax.set_zlabel('Principal Component 3')
-
-    cb = plt.colorbar(scatter)
-    cb.set_label('Cluster Label')
+    legend1 = ax.legend(*scatter.legend_elements(), loc="best", title="Classes")
+    ax.add_artist(legend1)
     plt.title('Predicted clusters')
     
     
     ax2 = fig.add_subplot(122, projection='3d')
-
     scatter2 = ax2.scatter(trans_data_pca[:, 0], trans_data_pca[:, 1],
                          trans_data_pca[:, 2], c=target, cmap='viridis', s=50)
     ax2.set_xlabel('Principal Component 1')
     ax2.set_ylabel('Principal Component 2')
     ax2.set_zlabel('Principal Component 3')
     
-    cb2 = plt.colorbar(scatter2)
-    cb2.set_label('Cluster Label')
-
+    legend2 = ax2.legend(*scatter2.legend_elements(), loc="best", title="Classes")
+    ax2.add_artist(legend2)
     plt.title('Actual Clusters')
+
     plt.show()
     
     
